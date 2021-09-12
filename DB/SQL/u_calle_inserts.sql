34685195a566c7b5798d097fdadfa81e5a4717a7
bryansbr <bryan.biojo@correounivalle.edu.co>
2021-09-12 17:06:31 -0500

    insertando más datos a la bd

--- DB/SQL/u_calle_inserts.sql
+++ DB/SQL/u_calle_inserts.sql
@@ -5,50 +5,129 @@
 INSERT INTO `u_calle`.`Persona`
               (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
               `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
-VALUES (2403112, 'Amilcar', 'Soteldo', '1990-01-17', 'M', 'eduar.soteldo@ucalle.edu.co', 
-       'Calle 13 # 80-12', 3184357389, 'Venezolano', 'jamilcarsd', SHA('profsoteldo19'), '2019-03-21');
+VALUES (2403112, 'Gregorio', 'Soteldo', '1990-01-17', 'M', 'gregogrio.soteldo@ucalle.edu.co', 
+       'Cll 13 # 80-12', 3184357389, 'VEN', 'gregoriosd', SHA('profsoteldo19'), '2019-03-21');
 
 INSERT INTO `u_calle`.`Persona`
               (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
               `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
 VALUES (5755754, 'Luis', 'Caicedo', '2002-06-29', 'M', 'luis.caicedo@ucalle.edu.co', 
-       'Cr 100 A oeste 1c-41', 3184335890, 'Colombiano', 'leduardocm', SHA('1007560Luis'), '2015-05-20');
+       'Cra 100 A oeste 1c-41', 3184335890, 'COL', 'luispc', SHA('1007560Luis'), '2015-05-20');
        
 INSERT INTO `u_calle`.`Persona`
               (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
               `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
 VALUES (2626266, 'Darwin', 'Alvarez', '2003-03-15', 'M', 'darwin.alvarez@ucalle.edu.co', 
-       'Av6 oeste 41-49', 3127231185, 'Colombiano', 'darwinsav', SHA('Nomeimporta.com'), '2021-09-10');
+       'Av 6 oeste 41-49', 3127231185, 'COL', 'darwinav', SHA('Nomeimporta.com'), '2021-09-10');
 
 INSERT INTO `u_calle`.`Persona`
               (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
               `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
 VALUES (1234567, 'Oscar', 'Bedoya', '1980-08-07', 'M', 'oscar.bedoya@ucalle.edu.co', 
-       'Calle 100 # 10-05', 3152679201, 'Colombiano', 'oscarfbl', SHA('oscarbed123'), '2015-07-17');
+       'Cll 100 # 10-05', 3152679201, 'COL', 'oscarbl', SHA('oscarbed123'), '2015-07-17');
 
 INSERT INTO `u_calle`.`Persona`
               (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
               `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
 VALUES (7654321, 'Bryan', 'Biojo', '1997-08-19', 'M', 'bryan.biojo@ucalle.edu.co', 
-       'Cra 25 # 5C-17', 3156649362, 'Colombiano', 'bryansbr', SHA('profbryan18'), '2018-02-05');
+       'Cra 25 # 5C-17', 3156649362, 'COL', 'bryanbr', SHA('profbryan18'), '2018-02-05');
 
 INSERT INTO `u_calle`.`Persona`
               (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
               `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
 VALUES (3423726, 'Guillermo', 'Hernandez', '1983-12-27', 'M', 'guillermo.hernandez@ucalle.edu.co', 
-       'Cra 86 # 80-10', 3173483240, 'Colombiano', 'guillermohnz', SHA('profguillermo18'), '2018-02-05');
+       'Cra 86 # 80-10', 3173483240, 'COL', 'guillermohz', SHA('profguillermo18'), '2018-02-05');
 
 INSERT INTO `u_calle`.`Persona`
               (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
               `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
 VALUES (5842219, 'Monica', 'Moreno', '1988-06-02', 'F', 'monica.moreno@ucalle.edu.co', 
-       'Calle 27 # 35-07', 3167987542, 'Colombiano', 'mariammg', SHA('profmonica13'), '2013-04-27');
+       'Cll 27 # 35-07', 3167987542, 'COL', 'monicamg', SHA('profmonica13'), '2013-04-27');
 
 INSERT INTO `u_calle`.`Persona`
               (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
               `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
 VALUES (2345437, 'Lina', 'Ruiz', '1993-05-13', 'F', 'lina.ruiz@ucalle.edu.co', 
-       'Av 2N # 02-23', 3203915382, 'Colombiano', 'linavrm', SHA('proflina15'), '2015-05-20');
+       'Av 2N # 02-23', 3203915382, 'COL', 'linavr', SHA('proflina15'), '2015-05-20');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (1264457, 'Antonia', 'Rodriguez', '1985-06-13', 'F', 'antonia.rodriguez@ucalle.edu.co', 
+       'Cll 110 # 20-37', 3224514381, 'ESP', 'antoniarp', SHA('profantonia11'), '2011-01-02');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (1264457, 'Carla', 'Venegas', '1994-09-16', 'F', 'carla.venegas@ucalle.edu.co', 
+       'Trv 103 # 44-17', 3111234567, 'CHL', 'carlavs', SHA('profcarla20'), '2020-08-12');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (1264457, 'Richard', 'Aguirre', '1970-06-20', 'M', 'richard.aguirre@ucalle.edu.co', 
+       'Cll 86 # 57-13', 3002304571, 'COL', 'richardac', SHA('profrichard07'), '2021-05-24');
+
+-- EST
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (3921028, 'Mariana', 'Rosario', '1999-02-19', 'F', 'mariana.rosario@ucalle.edu.co', 
+       'Cra 1 # 10-16', 3024832013, 'VEN', 'marianarb', SHA('semeolvido2021'), '2021-05-24');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (8674843 'Nicolas', 'Quintero', '2001-05-03', 'M', 'nicolas.quintero@ucalle.edu.co', 
+       'Dig 25 # 12-09', 3046859271, 'COL', 'nicolasqp', SHA('nicolasq21'), '2021-05-24');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (3230911, 'Aurimar', 'Suarez', '1997-10-27', 'F', 'aurimar.suarez@ucalle.edu.co', 
+       'Cll 30 # 30-02', 3103493121, 'VEN', 'aurimarst', SHA('aurimars21'), '2021-05-24');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (5388785, 'Kevin', 'Perdomo', '2000-12-08', 'M', 'kevin.perdomo@ucalle.edu.co', 
+       'Cra 56 # 12-21', 3156936353, 'COL', 'kevinjp', SHA('kevinp21'), '2021-05-24');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (4346337, 'Valeria', 'Padilla', '2002-07-21', 'F', 'valeria.padilla@ucalle.edu.co', 
+       'Cra 56 # 12-22', 3158727567, 'COL', 'valeriapp', SHA(' valeriap21'), '2021-05-24');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (4781242, 'Snaider', 'Pardo', '1998-08-31', 'M', 'snaider.pardo@ucalle.edu.co', 
+       'Cra 30 # 33-33', 3226153354, 'COL', 'snaiderpq', SHA('snaiderp21'), '2021-05-24');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (6563465, 'Monica', 'Materano', '1997-08-20', 'F', 'monica.materano@ucalle.edu.co', 
+       'Cra 80 # 70-16', 3123979419, 'VEN', 'monicamc', SHA('monicam21'), '2021-05-24');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (1976918, 'Tatiana', 'Caracas', '1997-11-15', 'F', 'tatiana.caracas@ucalle.edu.co', 
+       'Cra 80 # 60-20', 3122795666, 'COL', 'tatianacv', SHA('tatianac21'), '2021-05-24');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (3983655, 'Andres', 'Flores', '2003-12-21', 'M', 'andres.flores@ucalle.edu.co', 
+       'Calle 98 # 14-11', 3004562378, 'PER', 'andresfg', SHA('andresf20'), '2020-02-05');
+
+INSERT INTO `u_calle`.`Persona`
+              (`id_persona`, `nombre`, `apellido`, `fech_nac`, `genero`, `correo`, `direccion`, 
+              `celular`, `nacionalidad`, `nom_usuario`, `contrasena`, `creado_en`)
+VALUES (2858144, 'Sophia', 'Walker', '2004-08-10', 'F', 'sophia.walker@ucalle.edu.co', 
+       'Calle 117 # 30-20', 3156648790, 'USA', 'sophiaw', SHA('sophiaw21'), '2021-05-24');
 
 -- -----------------------------------------------------
 -- TABLA ADMINISTRADOR
@@ -62,16 +141,30 @@ INSERT INTO `u_calle`.`Administrador` (`id_adm`, `id_persona`) VALUES (1, 123456
 
 INSERT INTO `u_calle`.`Profesor`
               (`id_profesor`, `profesion`, `id_persona`, `id_adm`)
-VALUES (502, 'Historiador', 2403112, 1);
+VALUES (502, 'HIST', 2403112, 1);
 
 INSERT INTO `u_calle`.`Profesor`
               (`id_profesor`, `profesion`, `id_persona`, `id_adm`)
-VALUES (707, 'Ingeniero de Sistemas', 7654321, 1);
+VALUES (707, 'ING. SIS', 7654321, 1);
 
 INSERT INTO `u_calle`.`Profesor`
               (`id_profesor`, `profesion`, `id_persona`, `id_adm`)
-VALUES (523, 'Ingeniero de Sistemas', 3423726, 1);
+VALUES (523, 'ING. SIS', 3423726, 1);
 
 INSERT INTO `u_calle`.`Profesor`
               (`id_profesor`, `profesion`, `id_persona`, `id_adm`)
-VALUES (351, 'Ingeniera Industrial', 2345437, 1);
+VALUES (351, 'ING. IND', 2345437, 1);
+
+INSERT INTO `u_calle`.`Profesor`
+              (`id_profesor`, `profesion`, `id_persona`, `id_adm`)
+VALUES (176, 'MAT', 1264457, 1);
+
+INSERT INTO `u_calle`.`Profesor`
+              (`id_profesor`, `profesion`, `id_persona`, `id_adm`)
+VALUES (897, 'MAT', 1264457, 1);
+
+INSERT INTO `u_calle`.`Profesor`
+              (`id_profesor`, `profesion`, `id_persona`, `id_adm`)
+VALUES (095, 'FIS', 1264457, 1);
+
+
